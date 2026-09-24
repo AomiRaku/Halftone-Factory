07ca67258a69da61599bba330f85431db7fb1321
AomiRaku <33995671+AomiRaku@users.noreply.github.com>
2026-09-24 11:19:40 +0800

    测试效果

--- factory-main.js
+++ factory-main.js
@@ -779,7 +779,7 @@ const showConfirm = (message, opts = {}) => new Promise((resolve) => {
   cancel.addEventListener('click', () => cleanup(false));
 });
 
-const APP_VERSION = 'build 4321';
+const APP_VERSION = 'build 4322';
 
 // 关于对话框
 const showAbout = () => {
